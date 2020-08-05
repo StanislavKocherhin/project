0eb4d88a4a84893bcc471f7ad7a6ac054635b0a3
Stanislav Kocherhin <kamuflet99@gmail.com>
2020-08-05 11:48:56 +0300

    first comitt

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+// console.log(12);
+// let number = 24;
+// console.log(number);
+// const SumNumberCount = 3;
+// console.log(SumNumberCount);
+// var point = 5;
+// console.log(point);
+            // урок по класификации типов данних 
+// var obj = {
+//     name: "Jhon",
+//     age: 27,
+//     married: true
+// };
+// console.log(obj.married);
+// var arr = [1, 2, 3, 4 ];
+// console.log(arr[0]);
+// alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result); 
+// const answer = +prompt("Вам есть 18?", "16");
+// console.log(typeof(answer + 5));
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Какая ваша фамилая?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+// document.write (answers);
+
+    // const user = 'Stas';
+    // alert(`Привет, ${user}`);
+
+// const category = 'toys';
+// console.log(`https:udemy.com/${category}/5`);
+// console.log(2*3 && 5+1 == '6');
