6643da403a4895e8e0ff3c01fb9efb94973e7653
Stanislav Kocherhin <kamuflet99@gmail.com>
2020-09-15 12:05:12 +0300

    Fifth commit

--- JS/script.js
+++ JS/script.js
@@ -53,3 +53,40 @@
 // personalMovieDB.movies[c] = d;
 
 // console.log(personalMovieDB);
+
+//13 Урок
+
+if (4==4) {
+    console.log('Everything is Ok');
+}else {
+    console.log('Error');
+}
+
+// const num = 50;
+// if (num < 49){
+//     console.log('Error');
+// }else if(num > 100){
+//     console.log('To Much');
+// }else {
+//     console.log('Everything is Ok');
+// }
+
+//Тернарный оператор
+// (num == 50) ? console.log('Everything is Ok') : console.log('Error');
+
+// const num = 50;
+
+// switch (num){
+//     case 49:
+//         console.log('Wrong');
+//     break;
+//     case 100:
+//         console.log('Wrong');
+//     break;
+//     case 50:
+//         console.log('Right');
+//     break;
+//     default:
+//         console.log('Нет совпадений');
+//     break;
+// }
